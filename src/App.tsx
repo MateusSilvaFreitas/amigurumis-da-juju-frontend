3a5ff0a8164d06ed3609062009b530331f7d986b
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2025-11-08 20:27:57 -0300

    updated base url

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/header/Header";
 import { Homepage } from "./pages/homepage/Homepage";
 import "./App.css";
-import { Footer } from "./components/footer/footer";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
   return (
