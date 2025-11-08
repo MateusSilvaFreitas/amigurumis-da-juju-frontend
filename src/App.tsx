b1da8d85c36c665dbab8a0d5a86dfef99f55f7b9
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2025-11-08 20:36:53 -0300

    Testes configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./components/header/Header";
 import { Homepage } from "./pages/homepage/Homepage";
 import "./App.css";
@@ -9,11 +9,13 @@ function App() {
     <>
       <div className="geral-container">
         <Header />
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="/store" element={<p>Store Page</p>} />
-          <Route path="/amigurumis-da-juju-frontend" element={<p>Teste</p>} />
-        </Routes>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="/store" element={<p>Store Page</p>} />
+            <Route path="/amigurumis-da-juju-frontend" element={<p>Teste</p>} />
+          </Routes>
+        </BrowserRouter>
         <Footer />
       </div>
     </>
