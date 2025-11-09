29c9e09f3c375daa8c71e221decad1e71591d9da
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2025-11-08 22:24:00 -0300

    test 404

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), {
+      name: "copy-404",
+      closeBundle() {
+        fs.copyFileSync("dist/index.html", "dist/404.html");
+      },
+    }],
   base: "/",
 });
